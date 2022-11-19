f7b0292b4e9230ce2cedbb0b9f3b3b7253a2ff04
ogoretskyi <ogoretskyi@eliteit.org>
2022-11-19 14:05:21 +0200

    redux installed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-rxjs';
+	counter = 0;
+	updatedAt?: number;
+
+	get cannotDecrease(): boolean {
+		return this.counter <= 0;
+	}
+
+	increase(): void {
+		this.updatedAt = Date.now();
+		this.counter++;
+	}
+
+	decrease(): void {
+		this.updatedAt = Date.now();
+		this.counter--;
+	}
+
+	clear(): void {
+		this.updatedAt = Date.now();
+		this.counter = 0;
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { reducers, metaReducers } from './reducers';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		StoreModule.forRoot(reducers, {
+			metaReducers
+		}),
+		StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+		EffectsModule.forRoot([]),
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
